f50b8c8f1215e974571d72a941d7a3fb244e37e3
Dorad89 <doradulqinaku1989@gmail.com>
2023-11-01 22:45:59 +0100

    Initial commit

---
+++ src/main/java/dori89/tripsplanapplication/TripsPlanApplication.java
@@ -0,0 +1,13 @@
+package dori89.tripsplanapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripsPlanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripsPlanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dori89/tripsplanapplication/TripsPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package dori89.tripsplanapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripsPlanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
